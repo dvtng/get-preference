e204ebbc0a5c60d7095a4feddee4672b332cb435
David Tang <tang.ddt@gmail.com>
2019-11-14 11:24:08 -0800

    Vote breakdown

--- src/screens/PollResultsScreen.tsx
+++ src/screens/PollResultsScreen.tsx
@@ -1,20 +1,23 @@
-import React, { FC } from "react";
+import React, { FC, Fragment, useState } from "react";
 import { PollOption } from "../widgets/PollOption";
 import { Button } from "../widgets/Button";
 import { Screen } from "../features/Screen";
 import { useHistory } from "react-router-dom";
 import { PollState } from "../models/PollState";
 import { getPreferentialRanking } from "../utilities/getPreferentialRanking";
+import { Popup } from "../widgets/Popup";
 
 export type PollResultsProps = {
   poll: PollState;
 };
 
 export const PollResultsScreen: FC<PollResultsProps> = ({ poll }) => {
+  const history = useHistory();
+  const [isBreakdownOpen, setIsBreakdownOpen] = useState(false);
+
   const { counts, ranking } = getPreferentialRanking(
     Object.values(poll.votes || {})
   );
-  const history = useHistory();
 
   return (
     <Screen
@@ -45,6 +48,44 @@ export const PollResultsScreen: FC<PollResultsProps> = ({ poll }) => {
           />
         );
       })}
+      <div
+        style={{
+          height: 56,
+          padding: 16,
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <Button
+          onClick={() => {
+            setIsBreakdownOpen(true);
+          }}
+        >
+          See vote breakdown
+        </Button>
+      </div>
+      <Popup
+        isOpen={isBreakdownOpen}
+        actions={
+          <Button
+            onClick={() => {
+              setIsBreakdownOpen(false);
+            }}
+          >
+            Close
+          </Button>
+        }
+      >
+        <h2>Vote breakdown</h2>
+        {Object.entries(poll.votes || {}).map(([userId, votes]) => (
+          <Fragment key={userId}>
+            <h3>{poll.users[userId].name}</h3>
+            {votes.map(optionId => (
+              <p key={optionId}>{poll.options[optionId].label}</p>
+            ))}
+          </Fragment>
+        ))}
+      </Popup>
     </Screen>
   );
 };
