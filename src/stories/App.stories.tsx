2ebbd89af467cdb891bbaa084f14720b34755a22
David Tang <tang.ddt@gmail.com>
2019-11-14 19:37:42 -0800

    Fix app story

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { App } from "../App";
+import { Setup } from "./Setup";
 
 export default {
   title: "App"
 };
 
-export const app = () => <App />;
+export const app = () => (
+  <Setup>
+    {(db, currentUser) => {
+      currentUser.signOut();
+      return <App />;
+    }}
+  </Setup>
+);
